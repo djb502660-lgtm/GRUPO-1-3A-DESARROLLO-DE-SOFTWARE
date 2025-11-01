1053756e2d5ed9ad3b2e27b36fd093a0abca5de0
Amy Villalba <amyvillalba30@gmail.com>
2025-11-01 12:42:42 -0500

    Trabajo grupal

--- Proyectos/todolist/frontend/js/config.js
+++
@@ -1,4 +0,0 @@
-// Archivo de configuración para centralizar variables globales.
-
-// Define la ruta base de la API para que sea fácil de modificar en el futuro.
-const API_URL = "/xampp/GRUPO-1-3A-DESARROLLO-DE-SOFTWARE/Proyectos/todolist/backend/api/endpoint.php";
